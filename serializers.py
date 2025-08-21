205945d9b56aa82e9f4f0f2c721dbe7b96f2164c
LehMoreno8 <leticia.moreno2808@gmail.com>
2025-08-21 09:20:56 -0400

    serializers.py

--- serializers.py
+++ serializers.py
@@ -15,6 +15,24 @@ class EtiquetaSerializer(serializers.ModelSerializer):
 
 
 class TarefaSerializer(serializers.ModelSerializer):
+     grupo = GrupoSerializer(read_only=True)
+    etiquetas = EtiquetaSerializer(many= True, read_only=True)
+
+    grupo_id = serializers.PrimaryKeyRelatedField(
+        source='grupo',
+        queryset=Grupo.objects.all(),
+        write_only=True,
+        required=False
+    )
+    etiquetas_ids = serializers.PrimaryKeyRelatedField(
+        source='etiquetas',
+        queryset=Etiqueta.objects.all(),
+        many=True,
+        write_only=True,
+        required=False
+    )
+
     class Meta:
         model = Tarefa
         fields = '__all__'
+
